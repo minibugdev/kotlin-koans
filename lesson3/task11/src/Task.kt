fd66d1d406b3d472632f4d483c096fa64792d166
minibugdev <minibugdev@gmail.com>
2016-06-30 20:06:58 +0700

    completed Lesson3 (Collections)

--- lesson3/task11/src/Task.kt
+++ lesson3/task11/src/Task.kt
@@ -1,13 +1,7 @@
 // Return the most expensive product among all delivered products
 // (use the Order.isDelivered flag)
-fun Customer.getMostExpensiveDeliveredProduct(): Product? {
-    TODO()
-}
+fun Customer.getMostExpensiveDeliveredProduct(): Product? = orders.filter { it.isDelivered }.flatMap { it.products }.maxBy { it.price }
 
 // Return how many times the given product was ordered.
 // Note: a customer may order the same product for several times.
-fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
-    TODO()
-}
-
-/* TODO */
+fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int = customers.flatMap { it.orders.flatMap { it.products } }.count { it.equals(product) }
